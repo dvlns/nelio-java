80812fb7910ac22349efea7bfd696f9f51786bfb
PC <PC@DESKTOP-PHCHTJ1>
2023-08-02 21:06:22 -0300

    Primeiro commit

---
+++ teste/src/teste/Main.java
@@ -0,0 +1,12 @@
+package teste;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Write your age: ");
+		int age = sc.nextInt();
+		System.out.println("Your age is: "+age);
+	}
+	
+}
