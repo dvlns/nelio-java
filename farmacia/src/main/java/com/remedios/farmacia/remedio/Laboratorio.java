684bda4b59858ca33ae74653cc4c959f92bf3991
Davi Lins <141285951+dvlns@users.noreply.github.com>
2024-08-19 13:23:28 -0300

    Delete directory

--- farmacia/src/main/java/com/remedios/farmacia/remedio/Laboratorio.java
+++
@@ -1,7 +0,0 @@
-package com.remedios.farmacia.remedio;
-
-public enum Laboratorio {
-	
-	MEDELY,
-	ACHE
-}
--- farmacia/src/main/java/com/remedios/farmacia/remedio/RemedioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.remedios.farmacia.remedio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RemedioRepository extends JpaRepository<Remedio, Long>{
-
-}
